cb21507a5a432e8a07a72303aba001b17a101d9e
unknown <gabrieltorresmora2@gmail.com>
2022-12-16 12:42:52 -0300

    Mejorando el slider para verse en una sola pagina, y adaptando a dispotios mobiles, creacion de seccion de preguntas al final del inicio

--- public/js/menu/inicio.js
+++ public/js/menu/inicio.js
@@ -1,5 +1,56 @@
 async function renderPlantillaListado(listado) {
 
+    let slider = document.querySelector('.section-slider__contenedor')
+    let sliderIndividual = document.querySelectorAll('.section-slider__slider')
+    let contador = 1;
+    let width = sliderIndividual[0].clientWidth;
+    console.log(width)
+    let intervalo = 5000;
+    
+    window.addEventListener('resize', function(){
+        width = sliderIndividual[0].clientWidth;
+    })
+    
+    setInterval(function(){
+        slides();
+    },intervalo);
+    
+    function slides(){
+        slider.style.transform = "translate("+(-width*contador)+"px)";
+        slider.style.transition = "transform .5s";
+        contador++;
+    
+        if(contador == sliderIndividual.length){
+            setTimeout(function(){
+                slider.style.transform = "translate(0px)";
+                slider.style.transition = "transform 0s";
+                contador=1;
+            },5000)
+        }
+    }
+
+    (() => {
+        const titleQuestions = [...document.querySelectorAll('.questions__title')];
+        console.log(titleQuestions)
+    
+        titleQuestions.forEach(question =>{
+            question.addEventListener('click', ()=>{
+                let height = 0;
+                let answer = question.nextElementSibling;
+                let addPadding = question.parentElement.parentElement;
+    
+                addPadding.classList.toggle('questions__padding--add');
+                question.children[0].classList.toggle('questions__arrow--rotate');
+    
+                if(answer.clientHeight === 0){
+                    height = answer.scrollHeight;
+                }
+    
+                answer.style.height = `${height}px`;
+            });
+        });
+    })();
+
     try {
         
         const respuesta = await fetch('plantillas/inicio.hbs')
